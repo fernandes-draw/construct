b9b3b86db264ffc9ab397b3752baa76d52197b7f
Everson Fernandes <fernandes_draw@hotmail.com>
2026-01-27 23:24:22 -0300

    cadastrar vendedor

--- construct_youtube/settings.py
+++ construct_youtube/settings.py
@@ -105,7 +105,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "templates/static/"),)
+STATIC_ROOT = os.path.join("static")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # Config auth
 AUTH_USER_MODEL = "usuarios.Users"
